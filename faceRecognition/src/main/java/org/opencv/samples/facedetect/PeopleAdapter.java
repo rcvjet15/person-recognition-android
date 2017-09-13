7225f1086ab8aa6a7f53fe1e1b7c53de387f1fbe
Robert Cvjetković <robertcvjetkovic15@gmail.hr>
2017-09-13 23:57:19 +0200

    Add people adapter

---
+++ faceRecognition/src/main/java/org/opencv/samples/facedetect/PeopleAdapter.java
@@ -0,0 +1,107 @@
+package org.opencv.samples.facedetect;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.opencv.samples.facedetect.data.PersonContract.*;
+import org.opencv.samples.facedetect.utilities.ImageUtils;
+
+/**
+ * Created by Robi on 13/09/2017.
+ */
+
+public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.PeopleAdapterViewHolder> {
+
+    private final Context mContext;
+
+    private Cursor mCursor;
+
+    final private PeopleAdapterOnClickHandler mClickHandler;
+
+    public interface PeopleAdapterOnClickHandler{
+        void onClick(long id);
+    }
+
+    public PeopleAdapter(@NonNull Context context, PeopleAdapterOnClickHandler clickHandler){
+        mContext = context;
+        mClickHandler = clickHandler;
+    }
+
+    @Override
+    public PeopleAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater
+                .from(mContext)
+                .inflate(R.layout.person_list_item, viewGroup, false);
+        view.setFocusable(true);
+        return new PeopleAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PeopleAdapterViewHolder holder, int position) {
+        mCursor.moveToPosition(position);
+
+        long id = mCursor.getLong(
+                mCursor.getColumnIndexOrThrow(PersonEntry._ID));
+
+        String firstName = mCursor.getString(
+                mCursor.getColumnIndexOrThrow(PersonEntry.COLUMN_FIRST_NAME));
+
+        String lastName = mCursor.getString(
+                mCursor.getColumnIndexOrThrow(PersonEntry.COLUMN_LAST_NAME));
+
+        byte[] imageByteArr = mCursor.getBlob(
+                mCursor.getColumnIndexOrThrow(PersonEntry.COLUMN_PROFILE_PIC));
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(imageByteArr, 0, imageByteArr.length);
+        RoundedBitmapDrawable roundBitmap = RoundedBitmapDrawableFactory.create(mContext.getResources(), bitmap);
+        roundBitmap.setCircular(true);
+
+        holder.profilePic.setImageDrawable(roundBitmap);
+
+        String personSummary = String.format("%s %s", lastName, firstName);
+        holder.personSummary.setText(personSummary);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor != null ? mCursor.getCount() : 0;
+    }
+
+    void swapCursor(Cursor newCursor){
+        mCursor = newCursor;
+        notifyDataSetChanged();
+    }
+
+
+    class PeopleAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        final ImageView profilePic;
+        final TextView personSummary;
+
+        PeopleAdapterViewHolder(View view){
+            super(view);
+
+            profilePic = (ImageView) view.findViewById(R.id.personItemProfilePic);
+            personSummary = (TextView) view.findViewById(R.id.personItemData);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+
+        }
+    }
+}
--- faceRecognition/src/main/java/org/opencv/samples/facedetect/utilities/PeopleActivity.java
+++ faceRecognition/src/main/java/org/opencv/samples/facedetect/utilities/PeopleActivity.java
@@ -1,10 +1,45 @@
 package org.opencv.samples.facedetect.utilities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
 import org.opencv.samples.facedetect.BaseAppActivity;
+import org.opencv.samples.facedetect.PeopleAdapter;
+import org.opencv.samples.facedetect.R;
 
 /**
  * Created by Robi on 13/09/2017.
  */
 
-public class PeopleActivity extends BaseAppActivity {
+public class PeopleActivity extends BaseAppActivity implements PeopleAdapter.PeopleAdapterOnClickHandler {
+
+    private PeopleAdapter mPeopleAdapter;
+    private RecyclerView mRecyclerView;
+    private int mPosition = RecyclerView.NO_POSITION;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mPeopleAdapter = new PeopleAdapter(this, this);
+
+        mRecyclerView.setAdapter(mPeopleAdapter);
+
+    }
+
+    @Override
+    public void onClick(long id) {
+        Intent personShowIntent = new Intent(this, PeopleActivity.class);
+
+    }
 }
